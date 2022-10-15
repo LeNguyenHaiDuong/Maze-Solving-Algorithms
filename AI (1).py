5129c1cc0c787c95f756af954ba0f2eef35290eb
Tuyet3005 <84055975+Tuyet3005@users.noreply.github.com>
2022-10-15 18:46:15 +0700

    DFS + Greedy + Heuristic_3(tinh khoang cach Euclid, da chay thu H1 va H3 va thay co su khac biet)

--- AI (1).py
+++ AI (1).py
@@ -1,3 +1,6 @@
+import math
+
+
 class Node():
     def __init__(self, ele, pos):
         self.pre_node = []  # node truoc
@@ -93,6 +96,46 @@ class Map():
                 print(j.element, end='')
             print()
 
+# import stack_data
+stack = []
+m = Map()
+
+
+def DFS(goal, close):
+    if len(stack) != 0:
+        cur_node = stack.pop()
+        close.append(cur_node)
+
+        if cur_node is goal:
+            return
+
+        for node in cur_node.neighbor_node:
+            if node not in close:
+                node.pre_node.append(cur_node.self_node)
+                stack.append(node)
+                DFS(goal, close)
+
+
+def GreedySearch(start, goal):
+    open = [(start, 0)]
+    close = []
+
+    while goal not in close:
+        if open:
+            mini = min(open, key = lambda node: node[1])
+            open.remove(mini) 
+            cur_node = mini[0]
+            close.append(mini[0])  
+
+            for node in cur_node.neighbor_node:
+                cost = heuristic_3(node, goal)
+                if node not in close:
+                    open.append((node, cost))
+                    node.pre_node.append(cur_node.self_node)
+
+        else:
+            print('Can not find any way!')
+            break
 
 def UCS(start, goal, explore):
     mini = explore[0]
@@ -116,6 +159,10 @@ def UCS(start, goal, explore):
 
 def heuristic_1(goal, now):  # khoang cach theo toa do
     return abs(goal.self_node[0] - now.self_node[0]) + abs(goal.self_node[1] - now.self_node[1]) + now.self_cost
+
+
+def heuristic_3(goal, now):
+    return math.dist(now.self_node, goal.self_node)
 
 
 def heuristic_2(mat, now, next_node):  # giai thuat bam tuong ben phai
@@ -170,28 +217,34 @@ def output_result(now, map):
         now.element = 'o'
         return
     now.element = 'o'
-    td = now.pre_node.pop(0)
+    td = now.pre_node.pop()
     output_result(map.matrix[td[0]][td[1]], map)
 
 
 explore = []
 
-
 def main():
     # - In ra ket qua
-    map = Map()
-    map.read_file2('input.txt')
-    map.print_matrix()
+    m.read_file2('input.txt')
+    m.print_matrix()
 
-    explore.append(map.start_node)
-
-    UCS(map.start_node, map.end_node, explore)
-
-    output_result(map.end_node, map)
+    # explore.append(m.start_node)
+    # UCS(m.start_node, m.end_node, explore)
+    # output_result(m.end_node, m)
 
     print()
+    # TEST THUAT TOAN DFS
+    # stack.append(m.start_node)
+    # close = []
+    # close.append(m.start_node)
+    # DFS(m.end_node, close)
+    # output_result(m.end_node, m)
 
-    map.print_matrix()
+    # TEST THUAT TOAN GREEDY BEST FIRST SEARCH
+    GreedySearch(m.start_node, m.end_node)
+    output_result(m.end_node, m)
+
+    m.print_matrix()
 
 
 main()
